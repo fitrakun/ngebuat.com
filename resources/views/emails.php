697712d37268a8b1422a52920766ea0e6176f9c9
xpanda17 <aileeans75@yahoo.com>
2016-12-27 02:21:46 +0700

    t

--- resources/views/emails.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-	Hi, selamat telah berhasil mendaftar pada www.ngebuat.com<br>
-	silahkan kunjungi www.ngebuat.com/confirm/{{$user->confirmation_token}} untuk menyelesaikan proses registrasi<br>
-</body>
-</html>
